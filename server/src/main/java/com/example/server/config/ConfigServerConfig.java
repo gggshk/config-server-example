1634fee54694f83b1897a98216c13389119975b5
gggshk <pocal777@naver.com>
2023-10-08 00:08:02 +0900

    구성 서버 public 저장소 기본 설정

---
+++ server/src/main/java/com/example/server/config/ConfigServerConfig.java
@@ -0,0 +1,10 @@
+package com.example.server.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+public class ConfigServerConfig {
+
+}
