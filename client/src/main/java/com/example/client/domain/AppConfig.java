6cfc616826e178d3cce1e6c8c895e222aaee19b6
gggshk <pocal777@naver.com>
2023-10-09 17:46:11 +0900

    /actuator/refresh 를 사용한 설정 정보 갱신

--- client/src/main/java/com/example/client/domain/AppConfig.java
+++ client/src/main/java/com/example/client/domain/AppConfig.java
@@ -2,12 +2,46 @@ package com.example.client.domain;
 
 import java.util.Map;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope //RefreshScope을 사용해서 갱신을 하기 위해서는 getter, setter 필요
 @ConfigurationProperties(prefix = "spring")
-public record AppConfig(
-  Map<String, Object> application,
-  Map<String, Object> config,
-  Map<String, Object> dataSource
-) {
+public class AppConfig {
 
+  private Map<String, Object> application;
+  private Map<String, Object> config;
+  private Map<String, Object> dataSource;
+
+  public Map<String, Object> getApplication() {
+    return application;
+  }
+
+  public void setApplication(Map<String, Object> application) {
+    this.application = application;
+  }
+
+  public Map<String, Object> getConfig() {
+    return config;
+  }
+
+  public void setConfig(Map<String, Object> config) {
+    this.config = config;
+  }
+
+  public Map<String, Object> getDataSource() {
+    return dataSource;
+  }
+
+  public void setDataSource(Map<String, Object> dataSource) {
+    this.dataSource = dataSource;
+  }
+
+  @Override
+  public String toString() {
+    return "AppConfig{" +
+      "application=" + application +
+      ", config=" + config +
+      ", dataSource=" + dataSource +
+      '}';
+  }
 }
